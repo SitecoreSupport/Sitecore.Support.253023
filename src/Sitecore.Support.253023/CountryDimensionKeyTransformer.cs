e45fb4435100003e649260af102ce4ca39df81b8
DK\msb <msb@sitecore.net>
2018-09-06 18:37:20 -0400

    Transform country code to uppercase to avoid collisions

---
+++ src/Sitecore.Support.253023/CountryDimensionKeyTransformer.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.ExperienceAnalytics.Api.Response.DimensionKeyTransformers;
+using Sitecore.Globalization;
+
+namespace Sitecore.Support.ExperienceAnalytics.Api.Response.DimensionKeyTransformers
+{
+    public class CountryDimensionKeyTransformer : IDimensionKeyTransformer
+    {
+        // Methods
+        public string Transform(string key, Language language)
+        {
+            return new CountryDimensionKeyTransformer().Transform(key.ToUpper(), language);
+        }
+
+        // Properties
+        public string UnknownLabel
+        {
+            get
+            {
+                return "[unknown country]";
+            }
+        }
+    }
+
+
+}
